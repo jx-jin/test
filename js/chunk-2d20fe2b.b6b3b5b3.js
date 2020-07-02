ed4c1329d6177d4595a0557e83cc6ba9a2251a97
jx-jin <57336388+jx-jin@users.noreply.github.com>
2020-07-03 00:49:38 +0800

    Add files via upload

---
+++ js/chunk-2d20fe2b.b6b3b5b3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20fe2b"],{b612:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},c=[],r={},l=r,a=t("9ca4"),o=Object(a["a"])(l,u,c,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d20fe2b.b6b3b5b3.js.map
